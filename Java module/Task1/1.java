4ec01f802f7726073eab1780f4f1c74d79ab8bb1
PrasathV7781 <prasathv003@gmail.com>
2024-07-20 13:10:32 +0530

    Update 1.java

--- Java module/Task1/1.java
+++ Java module/Task1/1.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		//Read input from user using Scannner class Object
+		Scanner obj=new Scanner(System.in);
+		//Read the starting number
+		System.out.println("Enter the starting number: ");
+		int start=obj.nextInt();
+		//Read the starting number
+		System.out.println("Enter the ending number: ");
+		int end=obj.nextInt();
+		while(start<=end){
+		    System.out.print(start+" ");
+		    start++;
+		}
+		
+	}
+}
