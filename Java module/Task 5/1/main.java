2925226657468bda22172d49b92912cf7e3c0506
PrasathV7781 <prasathv003@gmail.com>
2024-08-14 19:15:47 +0530

    Update main.java

--- Java module/Task 5/1/main.java
+++ Java module/Task 5/1/main.java
@@ -1,1 +1,23 @@
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream<String> names = Stream.of("aBc", "d", "ef");
+
+        // Use map() to convert each string to uppercase
+        List<String> upperCaseNames = names.map(String::toUpperCase)
+                                           .collect(Collectors.toList());
+
+        // Print the result
+        upperCaseNames.forEach(System.out::println);
+    }
+}
+
+
+//Output:
+/*"C:\Program Files\Java\jdk-22\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1.4\lib\idea_rt.jar=61759:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1.4\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\PRASATH\IdeaProjectsTask\Task 5\out\production\Task 5" Main
+ABCDEF
+Process finished with exit code 0 ***/
 
