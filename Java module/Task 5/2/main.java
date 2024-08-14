18198940ec5f46221059e799f9d32ec3a4cacd35
PrasathV7781 <prasathv003@gmail.com>
2024-08-14 19:20:50 +0530

    Update main.java

--- Java module/Task 5/2/main.java
+++ Java module/Task 5/2/main.java
@@ -1,1 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) {
+        // Given list of strings
+        List<String> strings = Arrays.asList("abc", "", "be", "efg", "abcd", "", "jkl");
+
+        // Filter out empty strings
+        List<String> nonEmptyStrings = strings.stream()
+                                              .filter(s -> !s.isEmpty())
+                                              .collect(Collectors.toList());
+
+        // Print the list of non-empty strings
+        System.out.println(nonEmptyStrings);
+    }
+}
+
+//Output: [abc, be, efg, abcd, jkl]
