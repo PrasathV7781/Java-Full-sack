ebb6eb0ca81372ade10a08b1b457ebd6efd6e006
PrasathV7781 <prasathv003@gmail.com>
2024-12-30 18:48:47 +0530

    Update 6.java

--- Java module/Task1/6.java
+++ Java module/Task1/6.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-		//Read input from user using Scannner class Object
-		Scanner obj=new Scanner(System.in);
-		for(int i=5;i>0;i--){
-		    for(int j=5;j>i;j--){
-		        System.out.print(j);
-		    }for(int k=i;k>0;k--){
-		        System.out.print(k);
-		    }System.out.println();
-		}
-}
-
+public class PatternPrinter {
+    public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+	for(int i=0;i<n;i++) { 
+            for(int j=n;j>=1;j--) { 
+                if((i+j)>n) 
+                    System.out.print(j); 
+                else 
+                     System.out.print(n-i); 
+             } 
+         System.out.println(); 
+        } 
+    }
 }
