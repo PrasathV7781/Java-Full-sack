c73a42709e02fa478d046d920e47a0ad16bf0a03
PrasathV7781 <prasathv003@gmail.com>
2025-03-05 19:17:21 +0530

    Update Person.java

--- Java module/Task 2/1.1/Person.java
+++ Java module/Task 2/1.1/Person.java
@@ -2,7 +2,7 @@ public class Person {
     private String name;
     private int age;
 
-    // Constructor with name age
+    // Constructor with initialize name age
     public Person(String name) {
         this.name = name;
         this.age = 18;
